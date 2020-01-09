2e3ccea8de720eb21f9b10b6f7b2ea75ccf04262
xcd0 <xcd0@xcd0.com>
2020-01-09 10:42:26 +0900

    go_compiler_install.sh だいぶ形になった $GOPATHのチェックを実装したい

--- go_compiler_install.sh
+++ go_compiler_install.sh
@@ -6,18 +6,12 @@ GO_INSTALL_DIR=$HOME/work/go
 
 # インストールしたいバージョンに合わせる
 # go1.13.3のように頭にgoをつける
-#VERSION=go1.12.13
-
 VERSION=go1.13.5
-
-# シェルスクリプトの引数としてもいいかもしれない
-#VERSION=$1   # これだと先頭のgoの2文字を忘れるかも
-#$1の先頭2文字にgoがあるかどうか判定してゴニョニョしてもいい
 
 ################################################################################
 
 
-# 変数の規定値
+# その他変数の規定値
 OS=""
 ARCH="amd64"
 EXT="tar.gz"    # 拡張子
@@ -45,146 +39,72 @@ elif [ "$(expr substr $(uname -s) 1 4)" == "MSYS" ] \
 	fi
 fi # }}}
 
-while :
-do
-	cat << EOS
-Goのコンパイラを管理します。
-操作を入力してください。
-
-既定値 バージョン $VERSION  OS $OS Arch $ARCH インストール先 $GO_INSTALL_DIR
-
-s    : インストール済みのバージョン一覧を表示
-i    : $VERSION をインストール
-u    : $VERSION を削除
-r    : $VERSION を再インストール
-c    : コンパイラのバージョンを切り替える
-v    : 規定値のバージョンを変更
-o    : 規定値のインストールするOSを変更 (アーキテクチャも再設定)
-a    : 規定値のインストールするアーキテクチャを変更
-d    : 規定値のインストール先ディレクトリを変更
-p    : よく使うパッケージをインストール
-info : 仕組みを表示
-q    : 終了したい
-
-EOS
-	echo -n "[s/i/u/r/c/v/o/a/d/info/q] : "
-	read input1
-	echo ""
-	case $input1 in
-		"s")
-			showInstalledVersion()
-			;;
-		"i")
-			echo "インストール後にそのまま自動でよく使うパッケージをインストールしますか?"
-			echo -n "パッケージのインストールにはそれなりの時間がかかります。[y/N] :"
-			read input2
-			echo "コンパイラのインストールを開始します。"
-			goInstall
-			echo ""
-			if [ $? -eq 1 ]; then
-				# インストールしようとしたバージョンがすでにインストールされていた
-				# 再度プロンプトに戻す
-				continue
-			fi
-			;;
-		"u")
-			goUninstall
-			;;
-		"r")
-			echo "既存の $VERSION を削除して 再インストールします。"
-			goUninstall
-			if [ $? -eq 1 ]; then
-				# 削除しないを選んだ
-				# 再度プロンプトに戻す
-				continue
-			fi
-			# 削除したので再インストールする
-			goInstall
-			;;
-		"c")
-			# バージョンを切り替える
-			goChangeVersion()
-			;;
-		"v")
-			# 規定値のバージョンを書き換える
-			# 入力を受けてそれが存在するか確認し
-			# あればそれを適応する
-			while :
-			do
-				echo "バージョンを入力してください。"
-				echo "go1.13.5のように go*.*.* の形式で入力します。"
-				echo "バージョンの指定を中止する場合qと入力してください。"
-				echo -n "[go*.*.*/q] : "
-				read inputVersion
-				if [ "$inputVersion" == "q" ]; then
-					echo "終了します"
-					break
-				fi
-				echo "入力されたバージョン名 $inputVersion"
-				wget -q --spider --timeout 2 wget https://dl.google.com/go/${inputVersion}.${OS}-${ARCH}.${EXT}
-				if [ $? -eq 0 ]
-					# ある
-					VERSION=inputVersion
-					echo "$VERSION に切り替えました。"
-				else
-					# ない
-					echo "エラー : $inputVersion は存在しません。書式を見直してください。"
-					echo ""
-				fi
-			done
-			;;
-		"o")
-			changeOS()
-			changeArch()
-			;;
-		"a")
-			changeArch()
-			;;
-		"d")
-			# 規定値のインストール先ディレクトリを変更
-			goChangeDst()
-		"p")
-			# よく使うパッケージをインストール
-			goPackageInstall()
-		"info")
-			showProgramInfo()
-		"q")
-			echo "終了します。"
-			break
-			;;
-	esac
-done
-
-function showSupportInfo(){#{{{
+function showSupportInfo(){ #{{{
 	echo "サポートしているアーキテクチャ"
 	echo "OS        Architectures"
 	echo "FreeBSD   amd64, 386"
 	echo "Linux     amd64, 386, armv6l, arm64, ppc64le, s390x"
 	echo "macOS     amd64"
 	echo "Windows   amd64, 386"
-}#}}}
+} #}}}
 
-function showProgramInfo(){#{{{
-	cat << EOS
-仕組み
+function showProgramInfo(){ #{{{
+	cat << "EOS"
+# 仕組み
 
+## 前提
+以下の設定を.bashrcや.bash_profileなどに追記します。
+
+```
+GO_INSTALL_DIR=$HOME/work/go     <- これは任意 このシェルスクリプトにも設定する
+export GOPATH=$GO_INSTALL_DIR/go
+export GOBIN=$GOPATH/bin
+export GOROOT=$GOPATH/go
+export PATH=\GOBIN:$GOROOT/bin:$PATH
+```
+
+この.bashrcや.bash_profileの GO_INSTALL_DIR は好きなディレクトリを指定します。
+同様にこのシェルスクリプトの最初のほうに設定されている
+GO_INSTALL_DIR を設定ファイルで指定したディレクトリに合わせます。
+
+## 動作
+
+$GO_INSTALL_DIR以下にコンパイラをインストールします
+$GOPATH が $GO_INSTALL_DIR/go に設定されていれば、
+$GO_INSTALL_DIR/go がシンボリックリンクになっているため、
+バージョンを切り替えることができます。
 $GO_INSTALL_DIR 以下に公式サイトからコンパイラのバイナリファイルを
 ダウンロードしてバージョン名でフォルダを作って展開し、
-それに対して$GO_INSTALL_DIR/goからシンボリックリンクを張ります。
+それに対して $GO_INSTALL_DIR/go からシンボリックリンクを張ります。
 
 $GO_INSTALL_DIR/
-├── go               -> どれかへのシンボリックリンク
-├── go1.4.3          <- インストールしたコンパイラ
-└── go1.13.5         <- インストールしたコンパイラ
+│
+├── go -> go1.13.5    <- シンボリックリンク
+│
+├── go1.12.14
+│   ├── bin
+│   ├── go
+│   ├── pkg
+│   └── src
+├── go1.13.4
+│   ├── bin
+│   ├── go
+│   ├── pkg
+│   └── src
+└── go1.13.5
+    ├── bin
+    ├── go
+    ├── pkg
+    └── src
 
 EOS
-}#}}}
+} #}}}
 
-function changeArch(){#{{{
+function changeArch(){ # {{{
 	while :
 	do
 		echo "アーキテクチャを選択してください。"
-		showSupportInfo()
+		showSupportInfo
 		echo "現在指定されているOSは $OS です。"
 		if [ "$OS" == "freebsd" -o "$OS" == "windows"]; then
 			echo "1 : amd64"
@@ -192,16 +112,16 @@ function changeArch(){#{{{
 			echo "q : アーキテクチャの選択を終了 (規定値にリセット)"
 			echo -n "[1/2] : "
 			read $num
-			case $num
-				1)
+			case $num in
+				"1" )
 					ARCH=amd64
 					break
 					;;
-				2)
+				"2" )
 					ARCH=386
 					break
 					;;
-				"q")
+				"q" )
 					echo "アーキテクチャを規定値 amd64 に設定して"
 					echo "アーキテクチャの選択を終了します。"
 					ARCH=amd64
@@ -213,12 +133,12 @@ function changeArch(){#{{{
 			echo "q : アーキテクチャの選択を終了"
 			echo -n "[1/q] : "
 			read $num
-			case $num
-				1)
+			case $num in
+				"1" )
 					ARCH=amd64
 					break
 					;;
-				"q")
+				"q" )
 					echo "アーキテクチャを規定値 amd64 に設定して"
 					echo "アーキテクチャの選択を終了します。"
 					ARCH=amd64
@@ -235,28 +155,28 @@ function changeArch(){#{{{
 			echo "q : アーキテクチャの選択を終了"
 			echo -n "[1/2/q] : "
 			read $num
-			case $num
-				1)
+			case $num in
+				"1" )
 					ARCH=amd64
 					break
 					;;
-				2)
+				"2" )
 					ARCH=386
 					break
 					;;
-				3)
+				"3" )
 					ARCH=armv6l
 					break
 					;;
-				4)
+				"4" )
 					ARCH=arm64
 					break
 					;;
-				5)
+				"5" )
 					ARCH=s390
 					break
 					;;
-				6)
+				"6" )
 					ARCH=ppc64le
 					break
 					;;
@@ -269,9 +189,9 @@ function changeArch(){#{{{
 			esac
 		fi
 	done
-}#}}}
+} # }}}
 
-function changeOS(){#{{{
+function changeOS(){ # {{{
 	while :
 	do
 		echo "選択してください"
@@ -282,7 +202,7 @@ function changeOS(){#{{{
 		echo "q : 終了"
 		echo -n "[1/2/3/4] : "
 		read $num
-		case $num
+		case $num in
 			1)
 				OS=freebsd
 				break
@@ -305,7 +225,7 @@ function changeOS(){#{{{
 				;;
 		esac
 	done
-}#}}}
+} # }}}
 
 function goInstall(){ # {{{1
 	# インストール先
@@ -337,19 +257,56 @@ function goInstall(){ # {{{1
 function showInstalledVersion(){ #{{{
 	cd $GO_INSTALL_DIR
 	find . -maxdepth 1 -type d | sed -e '1d' | sed -e 's;./;;g'
-}#}}}
+} # }}}
 
 function goChangeVersion(){ #{{{
+
+	v=`showInstalledVersion`
+
+	ary=(`echo $v`)   # 配列に格納
+	#$echo ${#ary[@]}     # 配列の要素数を表示
+
+	echo "インストール済みのコンパイラは以下の通りです。"
+	echo "切り替えたいバージョンを左の数値で指定します。"
+	echo ""
+	for i in `seq 1 ${#ary[@]}`; do
+		echo "$i : ${ary[$i-1]}"
+	done
+
+	echo ""
+	echo -n "切り替え先を数値で入力してください。 : "
+	read num
+	expr $num + 1 > /dev/null 2>&1
+	RET=$?
+	if [ $RET -eq 0 ] && [ $num -ge 1 ] && [ $num -le ${#ary[@]} ]; then
+		# 数値 1以上配列の要素数以下
+		echo -n "${ary[$num-1]} でよろしいですか？ [y/N] : "
+		read no
+		if [ "$no" == "y" ]; then
+			VERSION=${ary[$num-1]}
+			echo "$VERSION に切り替えました。"
+		else
+			echo "バージョンの切り替えを中止しました。"
+			echo "$VERSION のままです。"
+			return
+		fi
+	else
+		# 数値でない
+		echo "エラー : $num は不正です。"
+		echo "バージョンの切り替えを中止しました。"
+		echo "$VERSION のままです。"
+		return
+	fi
+
 	# シンボリックリンクの張替えのみを行う
 	cd $GO_INSTALL_DIR
 	rm -rf go
 	echo ln -s ${VERSION} go
 	ln -s ${VERSION} go
-	echo "シンボリックリンクを張り替えました。"
 	return
-}#}}}
+} # }}}
 
-function goUninstall(){#{{{
+function goUninstall(){ # {{{
 	# アンインストール
 	echo -n "本当に $VERSION を削除しますか？ [y/N] : "
 	read deleteFlag
@@ -361,18 +318,24 @@ function goUninstall(){#{{{
 	else
 		return 1
 	fi
-}#}}}
+} # }}}
 
-function goChangeDst(){#{{{
+function goChangeDst(){ # {{{
 	echo "Goのコンパイラをインストールするディレクトリを指定します。"
 	echo "ここで一時的に変更するよりも、このシェルスクリプトを直接編集することを推奨します。"
+	echo "q と入力すると変更を中止します。"
 	echo -n "インストール先のパスを入力してください。 : "
 	read dst
+	if [ "$dst" == "q" ]; then
+		echo "操作を中止ししました。"
+		echo "インストール先は $GO_INSTALL_DIR のままです。"
+		return
+	fi
 	GO_INSTALL_DIR=$dst
 	echo "$GO_INSTALL_DIRに変更しました。"
-}#}}}
+} # }}}
 
-function goPackageInstall(){#{{{
+function goPackageInstall(){ # {{{
 	if [ "$input2" == "y" ]; then
 		if ! ( type "brew" > /dev/null 2>&1 ); then
 			brew install peco &
@@ -399,4 +362,136 @@ function goPackageInstall(){#{{{
 		git config --global ghq.root $GOPATH/src
 		echo "よく使うパッケージのインストールが完了しました。"
 	fi
-}#}}}
+} # }}}
+
+
+while : # メインループ {{{
+do
+	gover=`go version 2> /dev/null`
+	gover="使用中のGoのコンパイラのバージョン : $gover"
+	godst=`which go 2> /dev/null`
+	gover="$gover : $godst"
+	godst=`which go 2> /dev/null`
+	if [ $? -eq 1 ]; then
+		godst="not installed."
+		gover=""
+	fi
+	cat << EOS
+Goのコンパイラを管理します。 操作を入力してください。
+既定値 バージョン : $VERSION / OS : $OS / Arch : $ARCH / インストール先 : $GO_INSTALL_DIR
+$gover
+\$GOPATH : $GOPATH
+
+    s    : インストール済みリストを表示
+    i    : $VERSION をインストール
+    u    : $VERSION を削除
+    r    : $VERSION を再インストール
+    c    : コンパイラのバージョンを切り替える
+    v    : 規定値のバージョンを変更
+    o    : 規定値のOSを変更 (アーキテクチャも再設定)
+    a    : 規定値のアーキテクチャを変更
+    d    : 規定値のインストール先ディレクトリを変更
+    p    : よく使うパッケージをインストール
+    info : 仕組みを表示
+    q    : 終了
+
+EOS
+	echo -n "[s/i/u/r/c/v/o/a/d/p/info/q] : "
+	read input1
+	echo ""
+	echo "--------------------------------------------------------------------------------"
+	echo ""
+	case $input1 in
+		"s")
+			showInstalledVersion
+			;;
+		"i")
+			echo "インストール後にそのまま自動でよく使うパッケージをインストールしますか?"
+			echo -n "パッケージのインストールにはそれなりの時間がかかります。[y/N] :"
+			read input2
+			echo "コンパイラのインストールを開始します。"
+			goInstall
+			echo ""
+			if [ $? -eq 1 ]; then
+				# インストールしようとしたバージョンがすでにインストールされていた
+				# 再度プロンプトに戻す
+				continue
+			fi
+			;;
+		"u")
+			goUninstall
+			;;
+		"r")
+			echo "既存の $VERSION を削除して 再インストールします。"
+			goUninstall
+			if [ $? -eq 1 ]; then
+				# 削除しないを選んだ
+				# 再度プロンプトに戻す
+				continue
+			fi
+			# 削除したので再インストールする
+			goInstall
+			;;
+		"c")
+			# バージョンを切り替える
+			goChangeVersion
+			;;
+		"v")
+			# 規定値のバージョンを書き換える
+			# 入力を受けてそれが存在するか確認し
+			# あればそれを適応する
+			while :
+			do
+				echo "バージョンを入力してください。"
+				echo "go1.13.5のように go*.*.* の形式で入力します。"
+				echo "バージョンの指定を中止する場合qと入力してください。"
+				echo -n "[go*.*.*/q] : "
+				read inputVersion
+				if [ "$inputVersion" == "q" ]; then
+					echo "終了します"
+					break
+				fi
+				echo "入力されたバージョン名 $inputVersion"
+				wget -q --spider --timeout 2 wget https://dl.google.com/go/${inputVersion}.${OS}-${ARCH}.${EXT}
+				if [ $? -eq 0 ]; then
+					# ある
+					VERSION=inputVersion
+					echo "$VERSION に切り替えました。"
+				else
+					# ない
+					echo "エラー : $inputVersion は存在しません。書式を見直してください。"
+					echo ""
+				fi
+			done
+			;;
+		"o")
+			changeOS
+			changeArch
+			;;
+		"a")
+			changeArch
+			;;
+		"d")
+			# 規定値のインストール先ディレクトリを変更
+			goChangeDst
+			;;
+		"p")
+			# よく使うパッケージをインストール
+			goPackageInstall
+			;;
+		"info")
+			showProgramInfo
+			;;
+		"q")
+			echo "終了します。"
+			break
+			;;
+		* )
+			echo "$input1 は選択肢にありません。"
+			;;
+	esac
+	echo ""
+	echo "--------------------------------------------------------------------------------"
+	echo ""
+done # }}}
+
